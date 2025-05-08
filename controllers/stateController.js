5511a93543d73352d2fe14ae66398305b21da538
Arnulfo Escarpita <a_escarpita@mail.fhsu.edu>
2025-05-08 09:43:26 -0500

    added glitch site and test site to CORS options and made adjustments to controller

--- controllers/stateController.js
+++ controllers/stateController.js
@@ -13,9 +13,9 @@ try{
         const Statesfunfacts = await State.find();
    
         await statesData.forEach(states => {
-            for(i = 0; i < Statesfunfacts.length; i++){
+            for(let i = 0; i < Statesfunfacts.length; i++){
                 if(states.code === Statesfunfacts[i].stateCode){
-                        states.funfacts = Statesfunfacts[i].funFacts;
+                        states.funfacts = Statesfunfacts[i].funfacts;
                  
                 }
             } 
@@ -45,14 +45,14 @@ try{
         const statesData = await JSON.parse(data);
         const Statesfunfacts = await State.find();
         await statesData.forEach(states => {
-            for(i = 0; i < Statesfunfacts.length; i++){
+            for(let i = 0; i < Statesfunfacts.length; i++){
                 if(states.code === Statesfunfacts[i].stateCode){
-                        states.funfacts = Statesfunfacts[i].funFacts;
+                        states.funfacts = Statesfunfacts[i].funfacts;
                  
                 }
             } 
         });
-        stateCode = req.code;
+        let stateCode = req.code;
         let selectedState = await statesData.filter(states => {
             return states.code === stateCode;
         });
@@ -67,7 +67,7 @@ try{
         const data = await fsPromises.readFile(path.join(__dirname, '..', 'data', 'statesData.json'), 'utf8');
         const statesData = await JSON.parse(data);
         
-        stateCode = req.code;
+        let stateCode = req.code;
         let selectedState = await statesData.filter(states => {
             return states.code === stateCode;
         });
@@ -82,7 +82,7 @@ try{
         const data = await fsPromises.readFile(path.join(__dirname, '..', 'data', 'statesData.json'), 'utf8');
         const statesData = await JSON.parse(data);
     
-        stateCode = req.code;
+        let stateCode = req.code;
         let selectedState = await statesData.filter(states => {
             return states.code === stateCode;
         });
@@ -97,7 +97,7 @@ try{
         const data = await fsPromises.readFile(path.join(__dirname, '..', 'data', 'statesData.json'), 'utf8');
         const statesData = await JSON.parse(data);
 
-        stateCode = req.code;
+        let stateCode = req.code;
         let selectedState = await statesData.filter(states => {
             return states.code === stateCode;
         });
@@ -125,15 +125,15 @@ try{
         const statesData = await JSON.parse(data);
         const Statesfunfacts = await State.find();
         await statesData.forEach(states => {
-            for(i = 0; i < Statesfunfacts.length; i++){
+            for(let i = 0; i < Statesfunfacts.length; i++){
                 if(states.code === Statesfunfacts[i].stateCode){
-                        states.funfacts = Statesfunfacts[i].funFacts;
+                        states.funfacts = Statesfunfacts[i].funfacts;
                  
                 }
             } 
         });
 
-        stateCode = req.code;
+        let stateCode = req.code;
         let selectedState = await statesData.filter(states => {
             return states.code === stateCode;
         });
@@ -142,7 +142,7 @@ try{
         
         if(selectedState.funfacts){
 
-            randNum = Math.floor(Math.random() * (selectedState.funfacts.length));
+            let randNum = Math.floor(Math.random() * (selectedState.funfacts.length));
             res.json({"funfact": selectedState.funfacts[randNum]});
             
         }else{
@@ -152,43 +152,6 @@ try{
 
         
     }
-
-    //const createFunFact = async (req, res) =>{
-        //selectedStateCode = req.code;
-        //if(!req?.body?.funfacts || !Array.isArray(req.body.funfacts)){
-            //return res.status(400).json({'message': 'funfacts are required and must be an array'});
-        //}
-
-        //const selectedState = await State.findOne({stateCode: selectedStateCode}).exec();
-
-        //if(selectedState){
-
-           //let newFunfactArray =  selectedState.funFacts.concat(req.body.funfacts);
-
-           //selectedState.funFacts = newFunfactArray;
-            //const result = await selectedState.save();
-            //res.status(201).json(result);
-
-        //}else{
-
-            //try{
-                //console.log(req.body.funfacts);
-                //const result = await State.create({
-                    //stateCode: selectedStateCode,
-                    //funFacts: req.body.funfacts
-                //});
-
-                //res.status(201).json(result)
-
-            //}catch (err){
-                //console.error(err);
-            //}
-
-        //}
-
-
-        
-    //}
 
     const createFunFact = async (req, res) =>{
         let selectedStateCode = req.code;
@@ -206,7 +169,7 @@ try{
           
           for(let i = 0; i < newFunFacts.length; i++){
             
-            selectedState.funFacts.push(newFunFacts[i]);
+            selectedState.funfacts.push(newFunFacts[i]);
             
           }
             
@@ -218,7 +181,7 @@ try{
             try{
                 const result = await State.create({
                     stateCode: selectedStateCode,
-                    funFacts: newFunFacts
+                    funfacts: newFunFacts
                 });
 
                 res.json(result)
@@ -233,30 +196,7 @@ try{
         
     }
 
-    //const updateFunFact = async (req,res) =>{
-        //selectedStateCode = req.code;
-        //if(!req?.body?.funfact){
-            //return res.status(400).json({'message': 'A funfact is required'});
-        //}
-        //if(!req?.body?.index || !req.body.index > 0){
-            //return res.status(400).json({'message': 'index is required and must not be 0'});
-        //}
-
-        //const selectedState = await State.findOne({stateCode: selectedStateCode}).exec();  
-
-        //if(!selectedState){
-           // return res.status(400).json({'message': 'State does not have funfacts '});
-        //}else if (!selectedState.funFacts[req.body.index - 1]){
-            //return res.status(400).json({'message': 'State does not have a funfact in that position'});
-        //}else{
-            //selectedState.funFacts[req.body.index - 1] = req.body.funfact;
-            //const result = await selectedState.save();
-            //res.json(result);
-        //}
-        
-    //}
-
-    const updateFunFact = async (req,res) =>{
+   const updateFunFact = async (req,res) =>{
         const data = await fsPromises.readFile(path.join(__dirname, '..', 'data', 'statesData.json'), 'utf8');
         const statesData = await JSON.parse(data);
         let selectedStateCode = req.code;
@@ -280,49 +220,21 @@ try{
       
         let index = req.body.index - 1; 
 
-        console.log(index);
-
         const selectedState = await State.findOne({stateCode: selectedStateCode}).exec();  
 
         if(!selectedState){
             return res.status(400).json({'message': `No Fun Facts found for ${selectedStateInfo.state}`});
-        }else if (!selectedState.funFacts[index]){
+        }else if (!selectedState.funfacts[index]){
             return res.status(400).json({'message': `No Fun Fact found at that index for ${selectedStateInfo.state}`});
         }
-          selectedState.funFacts[index] = funfact;
+          selectedState.funfacts[index] = funfact;
           const result = await selectedState.save();
           res.json(result);
         
         
     }
 
-    //const deleteFunFact =  async (req, res) =>{
-        //selectedStateCode = req.code;
-        //if(!req?.body?.index || !req.body.index > 0){
-            //return res.status(400).json({'message': 'index is required and must not be 0'});
-        //}
-
-        //const selectedState = await State.findOne({stateCode: selectedStateCode}).exec();  
-
-        //if(!selectedState){
-            //return res.status(400).json({'message': 'State does not have funfacts '});
-        //}else if (!selectedState.funFacts[req.body.index - 1]){
-            //return res.status(400).json({'message': 'State does not have a funfact in that position'});
-        //}else{
-            
-            //let funfactToRemove = selectedState.funFacts[req.body.index - 1]
-            //let newfunfactArray = await selectedState.funFacts.filter(funfact => {
-                //return funfact != funfactToRemove;
-            //});
-
-            //selectedState.funFacts = newfunfactArray;
-
-            //const result = await selectedState.save();
-            //res.json(result);
-        //}
-    //}   
-
-        const deleteFunFact =  async (req, res) =>{
+   const deleteFunFact =  async (req, res) =>{
             const data = await fsPromises.readFile(path.join(__dirname, '..', 'data', 'statesData.json'), 'utf8');
             const statesData = await JSON.parse(data);
             let selectedStateCode = req.code;
@@ -345,24 +257,22 @@ try{
 
             if(!selectedState){
                 return res.status(400).json({'message': `No Fun Facts found for ${selectedStateInfo.state}`});
-            }else if (!selectedState.funFacts[index]){
+            }else if (!selectedState.funfacts[index]){
                 return res.status(400).json({'message': `No Fun Fact found at that index for ${selectedStateInfo.state}`});
             }
                 
-            let funfactToRemove = selectedState.funFacts[index]
-            let newfunfactArray = await selectedState.funFacts.filter(funfact => {
+            let funfactToRemove = selectedState.funfacts[index]
+            let newfunfactArray = await selectedState.funfacts.filter(funfact => {
                 return funfact != funfactToRemove;
             });
     
-            selectedState.funFacts = newfunfactArray;
+            selectedState.funfacts = newfunfactArray;
     
             const result = await selectedState.save();
             res.json(result);
             
         
         }
-    
-    
 
     module.exports = {
         getAllStates,
